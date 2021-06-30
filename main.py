7c64da4a689214ced8cfb10187e528b9f524c55e
npc203 <npc203@users.noreply.github.com>
2021-06-30 13:43:31 +0530

    nuke things

--- main.py
+++ main.py
@@ -7,31 +7,14 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./static/"
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/single", methods=["POST"])
-def single():
-    name = request.form["name"]
-    return serve_img(generate(name))
-
-
-def serve_img(pil_img):
-    img_io = BytesIO()
-    pil_img.save(img_io, "JPEG", quality=70)
-    img_io.seek(0)
-    return send_file(img_io, mimetype="image/jpeg")
-
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file1' not in request.files:
             return 'there is no file1 in form!'
         file1 = request.files['file1']
         if file1:  
-            path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], "certificate.png")
             file1.save(path)
         data = {"data":[]}
         with open( os.path.join(app.config['UPLOAD_FOLDER'], "info.json"),"w+") as f:
@@ -40,22 +23,34 @@ def upload_file():
                     tmp = j.split("|")
                     data["data"].append({"msg":tmp[0],"x":tmp[1],"y":tmp[2]})
             f.write(json.dumps(data,indent=4))
-
-        return f"{request}<br><br>{request.form}<br><br>{request.data})"
-    
+        return serve_img(generate())
     return render_template("modal.html")
 
-def generate(message):
+def serve_img(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, "JPEG", quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype="image/jpeg")
+
+def generate():
     image = Image.open("static/certificate.png")
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("static/Ubuntu-Regular.ttf", size=20)
-    color = "rgb(0,0,0)"
     with open("static/info.json","r") as f:
         data = json.load(f)
-    item = data["data"].pop(0)
-    draw.text((int(item['x']),int(item['y'])), message, fill=color, font=font)
+
+    #Aspect resize
+    basewidth = 800
+    wpercent = (basewidth/float(image.size[0]))
+    hsize = int((float(image.size[1])*float(wpercent)))
+    image = image.resize((basewidth,hsize), Image.ANTIALIAS)
+
+    #Drawing things
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("static/poppins.ttf", size=20)
+    color = "rgb(0,0,0)"
     for item in data["data"]:
         draw.text((int(item['x']),int(item['y'])), item["msg"], fill=color, font=font)
+
+    image = image.convert('RGB')
     return image
 
 
