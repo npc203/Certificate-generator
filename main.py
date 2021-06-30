98f15886b12533765a232de59d6266329eb4b9c2
npc203 <npc203@users.noreply.github.com>
2021-06-30 17:01:32 +0530

    added /list endpoint for bulk things

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import os,sys
 import json
+import zipfile
+import time
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./static/"
 
@@ -26,16 +29,49 @@ def upload_file():
         return serve_img(generate())
     return render_template("modal.html")
 
+@app.route('/list', methods=['GET','POST'])
+def list_use():
+    if request.method == 'POST':
+        # Making tmp cache dir
+        folder=str(int(time.time()))
+        path='tmp/'+folder
+        if not os.path.isdir('tmp'):
+            os.mkdir('tmp')
+        os.mkdir(path) 
+
+        # Setting up data
+        with open("static/info.json","r") as f:
+            data = json.load(f)
+        f=request.files['file']
+        content = f.read().decode("ascii").replace('\r','').split('\n')
+        zipf = zipfile.ZipFile('tmp/final.zip', 'w', zipfile.ZIP_DEFLATED)
+
+        # stuff
+        for raw_data in content:
+                parse  = raw_data.split(",")
+                fname=''.join(ch for ch in parse[0] if ch.isalnum())
+                for i in range(len(data["data"])):
+                    data["data"][i]["msg"] = parse[i]
+
+                generate(data).save(path+f'/{fname}.jpg')
+                zipf.write(os.path.join(path,f'{fname}.jpg'),arcname=f'{fname}.jpg')
+        zipf.close()
+
+        return send_file('tmp/final.zip', attachment_filename='Certificates.zip',as_attachment=True)
+    return render_template("list.html")
+
+
 def serve_img(pil_img):
     img_io = BytesIO()
     pil_img.save(img_io, "JPEG", quality=70)
     img_io.seek(0)
     return send_file(img_io, mimetype="image/jpeg")
 
-def generate():
+def generate(data=None):
     image = Image.open("static/certificate.png")
-    with open("static/info.json","r") as f:
-        data = json.load(f)
+    if not data:
+        with open("static/info.json","r") as f:
+            data = json.load(f)
 
     #Aspect resize
     basewidth = 800
